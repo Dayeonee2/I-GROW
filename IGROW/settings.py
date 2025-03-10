3b412b4c2925ab0694af4cf731768986720435a6
tlsekdus <tlsekdus77@naver.com>
2025-03-10 16:11:53 +0900

    Update settings for static files

--- IGROW/settings.py
+++ IGROW/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 추가
 ]
 
 ROOT_URLCONF = 'IGROW.urls'
