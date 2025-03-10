425e5cda3cdea1b08edc7d686674983b420bcaef
tlsekdus <tlsekdus77@naver.com>
2025-03-10 16:42:59 +0900

    ff

--- IGROW/settings.py
+++ IGROW/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)_)q+f+3!o0x*xyce83om6%vwh$zo&n$wde%*)a4-zbgp*lbpb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["i-grow.onrender.com", "www.igrowkorea.com", "igrowkorea.com"]
+ALLOWED_HOSTS = ["i-grow.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
